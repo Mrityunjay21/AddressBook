64cc92baf74948e0a534549ea5c93fb6595a2b30
Mrityunjay21 <mrityunjaynayak2021@gmail.com>
2022-12-05 17:13:39 +0530

    intial commit

--- src/Addressbook.java
+++ src/Addressbook.java
@@ -10,69 +10,72 @@ public class Addressbook {
 	private String emailid;
 	private int zipcode;
 	
-public void setFirstName(String pname) {
-	First_name = pname;
+public void setFirst_Name(String sname) {
+	First_name = sname;
 }
 public String getFirst_name() {
 	return First_name;
 }	
 
-public void setLast_name(String psname) {
-	Last_name = psname;
+public void setLast_name(String sLname) {
+	Last_name = sLname;
 }
-public String Last_name() {
+public String getLast_name() {
 	return Last_name;
 }	
-public void setAddress(String pAddress) {
-	Address = pAddress;
+public void setAddress(String sAddress) {
+	Address = sAddress;
 }
 public String getAddress() {
 	return Address;
 }	
-public void setcity(String pcity) {
-	First_name = pcity;
+public void setcity(String scity) {
+	city = scity;
 }
 public String getcity() {
 	return city;
 }	
-public void setstate(String pstate) {
-	state = pstate;
+public void setstate(String sstate) {
+	state = sstate;
 }
 public String getstate() {
 	return state;
 }	
-public void setPhoneno(String pPhoneno) {
-	Phoneno = pPhoneno;
+public void setPhoneno(String sPhoneno) {
+	Phoneno = sPhoneno;
 }
 public String getPhoneno() {
 	return Phoneno;
 }	
-public void setemailid(String pemailid) {
-	emailid = pemailid;
+public void setemailid(String semailid) {
+	emailid = semailid;
 }
 public String getemailid() {
 	return emailid;
 }	
-public void setzipcode(int pzipcode) {
-	zipcode = pzipcode;
+public void setzipcode(int szipcode) {
+	zipcode = szipcode;
 }
 public int getzipcode() {
 	return zipcode;
 }
 
+	
 
-	public static void main(String[]args) {
+
+	 public static void main(String[]args) {
 		
-		Addressbook mynewContent = new Addressbook();
-		Addressbook.setFirst_name(pname: "mrityunjay");
-		Addressbook.setLast_name(psname: "nayak");
-		Addressbook.setAddress(pAddress: "858 hbur nbugrb");
-		Addressbook.setcity(pcity: "bhopal");
-		Addressbook.Phoneno(pPhoneno:"987456226");
-		Addressbook.setemailid(pemailid:"jbjbb@jnini.com");
-		Addressbook.setzipcode(pzipcode:"456322");
+		Addressbook myAddressbook = new Addressbook();
+		myAddressbook.setFirst_Name( "mrityunjay");
+		myAddressbook.setLast_name( "nayak");
+		myAddressbook.setAddress( "858 hbur nbugrb");
+		myAddressbook.setcity( "bhopal");
+		myAddressbook.setPhoneno("987456226");
+		myAddressbook.setemailid("jbjbb@jnini.com");
+		myAddressbook.setzipcode(456322);
 		
-		System.out.println("Address : "+ Address.getFirst_Name);
+		System.out.println("myAddressbook  : "+ myAddressbook.getFirst_name()+ " "+ myAddressbook.getLast_name()+" "+myAddressbook.getAddress()+" "+ 
+				myAddressbook.getcity()+" " + myAddressbook.getPhoneno()+"  "+ myAddressbook.getemailid()+" "+ myAddressbook.getzipcode() );
 		
 	}
 }
